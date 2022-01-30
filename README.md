838fe37898b3f9e91e990f2c86b839e28fb532fa
Jake Barrett <55549269+JakeBGitHub@users.noreply.github.com>
2022-01-30 12:15:22 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# animated-octo-funicular
+
+[![run-workflow-A](https://github.com/JakeBGitHub/animated-octo-funicular/actions/workflows/workflow_A.yml/badge.svg)](https://github.com/JakeBGitHub/animated-octo-funicular/actions/workflows/workflow_A.yml)
---
+++ config/dwh_config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DRIVER = os.getenv("DRIVER")
+SERVER = os.getenv("SERVER")
+DATABASE = os.getenv("DATABASE")
+UID = os.getenv("UID")
+PASSWORD = os.getenv("PASSWORD")
+
+A_QUERY = os.getenv("A_QUERY")
